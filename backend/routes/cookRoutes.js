d16b3cf0fd8d9632d01609fa5fddcfd59c9cb1a0
Ankit <ankitsinghtanwar2008@gmail.com>
2026-07-23 00:14:56 +0530

    update cook file

--- backend/routes/cookRoutes.js
+++ backend/routes/cookRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Cook = require("../models/Cook");
+const Cook = require("../models/cook");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
